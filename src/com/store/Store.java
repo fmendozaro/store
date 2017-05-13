e2e30ce25bb0ed0f3464a89df2949d42e8813752
Fernando Mendoza <fernandoza@gmail.com>
2017-05-13 17:56:02 -0500

    Initial

---
+++ src/com/store/Store.java
@@ -0,0 +1,24 @@
+package com.store;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * Created by Fer on 5/13/2017.
+ */
+public class Store {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintStream out = new PrintStream(System.out);
+
+        showMenu(out);
+
+    }
+
+    public static void showMenu(PrintStream out){
+
+        out.println("Welcome to the Pinnacles Store");
+        out.println("Welcome to the Pinnacles Store");
+
+    }
+}
